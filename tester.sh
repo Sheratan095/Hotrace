8bceff2b594896d28ecdfac077f894e3af7ff469
LeBartol <lebartol@student.42firenze.it>
2025-05-25 15:13:18 +0200

    tester

--- tester.sh
+++ tester.sh
@@ -6,9 +6,8 @@ ECHO="echo"
 OUT="test_result.txt"
 #--exec
 X0="./hotrace"
-X1="./giacomo"
+X1="./Bombatomica64"
 X2="./tbeauzam"
-
 #--tests
 DIR="tests/resources"
 T1="$DIR/test01.htr"
@@ -18,8 +17,40 @@ T4="$DIR/test04.htr"
 T5="$DIR/test05.htr"
 T6="$DIR/test06.htr"
 T7="$DIR/test07.htr"
+T = ("$T1" "$T2" "$T3" "$T4" "$T5" "$T6" "$T7")
 
-$ECHO TESTER				> $OUT
+
+TEST_DIR="$PWD/test_repos"
+
+ROOT="$PWD"
+
+PWD=$(pwd)
+
+TESTED_REPOS=(
+	"https://github.com/tbeauzam/Rush_42_Hotrace.git"
+	"https://github.com/Bombatomica64/hotrace.git"
+)
+
+RESOURCES="../../tests/resources/"
+for REPO in "${TESTED_REPOS[@]}"; do
+	IFS='/'
+	read -ra newarr <<< "$REPO"
+	REPO_NAME="${newarr[3]%.git}"  # Remove .git from the end
+	git clone "$REPO" "$TEST_DIR/$REPO_NAME"
+	X+="$REPO_NAME"
+	cd "$TEST_DIR/$REPO_NAME" || exit
+	echo "Compiling $REPO_NAME..."
+	make
+	if [ $? -ne 0 ]; then
+		echo "Compilation failed for $REPO_NAME"
+		break
+	fi
+	mv hotrace "../../$REPO_NAME"
+done
+
+cd ../../
+
+$ECHO "TESTER"				> $OUT
 $ECHO "---TEST 1---"		>> $OUT
 $ECHO "Result for $X0"		>> $OUT
 bash -c "time $X0 < $T1"	2>> $OUT
