435c5eac8f053c98d512973bdc52a3cd55270af2
Caroline Wannheden <cwannhed@c2r1p9.42firenze.it>
2025-05-25 21:45:34 +0200

    normi

--- src/insertion.c
+++ src/insertion.c
@@ -3,42 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   insertion.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cwannhed <cwannhed@student.42firenze.it>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 14:57:52 by cwannhed          #+#    #+#             */
-/*   Updated: 2025/05/25 13:36:25 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/05/25 21:27:32 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Hotrace.h"
 
-static int	parse_line(t_data *data);
-
 int	insertion(t_data *data)
 {
-	return (parse_line(data));
-}
-
-static int	parse_line(t_data *data)
-{
 	size_t	line_len;
-	size_t	key_len;
-	char	*line = NULL;
-	char	*key = NULL;
+	char	*line;
+	char	*key;
 
+	line = NULL;
+	key = NULL;
 	line = get_next_line(0, FALSE);
 	while (line && line[0] != '\n')
 	{
 		line_len = ft_strlen(line);
 		line[line_len - 1] = '\0';
 		if (key == NULL)
-		{
 			key = line;
-			key_len = line_len;
-		}
 		else
 		{
-			add_entry(data, key, line, key_len - 1, line_len - 1);
+			add_entry(data, key, line, line_len - 1);
 			key = NULL;
 		}
 		line = get_next_line(0, FALSE);
